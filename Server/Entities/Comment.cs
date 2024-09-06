c15d84ecb6b2fdabef65f5ffc335d6e4812e84f5
KatAamand <katrine.aamand@icloud.com>
2024-09-06 13:25:43 +0200

    Adding entities and entity classes

---
+++ Server/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace Entities;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Body { get; set; }
+    public int AuthorId { get; set; }
+
+    public Comment(int id, string body, int authorId)
+    {
+        Id = id;
+        Body = body;
+        AuthorId = authorId;
+    }
+}
---
+++ Server/Entities/Post.cs
@@ -0,0 +1,18 @@
+﻿namespace Entities;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public int NoOfLikes { get; set; }
+    public List<Comment> ListOfComments { get; set; }
+
+    public Post(string title, string body)
+    {
+        Title = title;
+        Body = body;
+        NoOfLikes = 0; 
+        ListOfComments = new List<Comment>();
+    }
+}
