d89f19817b7ed4309f56ea2e39819a1f08b25648
KatAamand <katrine.aamand@icloud.com>
2024-09-10 08:48:52 +0200

    Updating test comments

--- Server/InMemoryRepositories/Test.cs
+++ Server/InMemoryRepositories/Test.cs
@@ -6,37 +6,35 @@ public class Test
 {
     public static void Main(string[] args)
     {
-        // Opret repositories med dummydata
+        // Opretter repositories med dummydata
         var postRepository = new InMemoryRepository<Post>();
         var userRepository = new InMemoryRepository<User>();
         var commentRepository = new InMemoryRepository<Comment>();
 
-        // Hent og udskriv dummydata for posts
+        // Henter og udskriver dummy data
         var posts = postRepository.GetManyAsync();
-        Console.WriteLine("Posts:");
+        Console.WriteLine("Posts: ");
         foreach (var post in posts)
         {
-            Console.WriteLine($"ID: {post.Id}, Title: {post.Title}, Body: {post.Body}");
+            Console.WriteLine($"Id: {post.Id}, Title: {post.Title}, Body: {post.Body}");
         }
 
         Console.WriteLine();
 
-        // Hent og udskriv dummydata for users
         var users = userRepository.GetManyAsync();
-        Console.WriteLine("Users:");
+        Console.WriteLine("Users: ");
         foreach (var user in users)
         {
-            Console.WriteLine($"ID: {user.Id}, Username: {user.Username}, Password: {user.Password}");
+            Console.WriteLine($"Id: {user.Id}, Username: {user.Username}, Password: {user.Password}");
         }
 
         Console.WriteLine();
 
-        // Hent og udskriv dummydata for comments
         var comments = commentRepository.GetManyAsync();
-        Console.WriteLine("Comments:");
+        Console.WriteLine("Comments: ");
         foreach (var comment in comments)
         {
-            Console.WriteLine($"ID: {comment.Id}, Post ID: {comment.PostId}, Body: {comment.Body}");
+            Console.WriteLine($"Id: {comment.Id}, PostID: {comment.PostId}, Body: {comment.Body}");
         }
     }
 }
