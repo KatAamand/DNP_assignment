c23ca197b7838f1460a360152a0bcaba31ae970a
KatAamand <katrine.aamand@icloud.com>
2024-09-17 20:00:21 +0200

    updating basic files

---
+++ Server/CLI/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CLI.UI;
+using Entities;
+using InMemoryRepositories;
+using RepositoryContracts;
+
+Console.WriteLine("Start CLI app");
+
+// Opretter repositories med dummydata
+IRepository<User> userRepository = new InMemoryRepository<User>();
+IRepository<Post> postRepository = new InMemoryRepository<Post>();
+IRepository<Comment> commentRepository = new InMemoryRepository<Comment>();
+
+CliApp cliApp = new CliApp(userRepository, postRepository, commentRepository); 
+await cliApp.StartAsync(); 
