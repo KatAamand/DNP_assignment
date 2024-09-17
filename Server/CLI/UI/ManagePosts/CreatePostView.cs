17e234ff553ecbdc5e68d8649e5f6830dab340ba
KatAamand <katrine.aamand@icloud.com>
2024-09-17 21:40:32 +0200

    Implementerer show metode i CreatePostView.cs

--- Server/CLI/UI/ManagePosts/CreatePostView.cs
+++ Server/CLI/UI/ManagePosts/CreatePostView.cs
@@ -6,9 +6,48 @@ namespace CLI.UI.ManagePosts;
 public class CreatePostView
 {
     private readonly IRepository<Post> repository;
+    private readonly IRepository<User> userRepository;
 
-    public CreatePostView(IRepository<Post> repository)
+    public CreatePostView(IRepository<Post> repository, IRepository<User> userRepository)
     {
         this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        this.userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+    }
+
+    public async Task Show()
+    {
+        Console.WriteLine("Opret ny post");
+        
+        Console.Write("Indtast titel: ");
+        string? title = Console.ReadLine();
+
+        while (string.IsNullOrWhiteSpace(title))
+        {
+            Console.WriteLine("Titel må ikke være tom, prøv igen");
+            title = Console.ReadLine();
+        }
+        
+        Console.Write("Indtast brugerID: ");
+        int authorId;
+        var users = (await userRepository.GetManyAsync()).ToList(); 
+
+        while (!int.TryParse(Console.ReadLine(), out authorId) || !users.Any(u => u.Id == authorId))
+        {
+            Console.WriteLine("Ugyldigt brugerID. Prøv igen.");
+        }
+
+        
+        Console.Write("Indtast indhold: ");
+        string? body = Console.ReadLine();
+        while (string.IsNullOrWhiteSpace(body))
+        {
+            Console.WriteLine("Indhold må ikke være tomt. Prøv igen.");
+            body = Console.ReadLine();
+        }
+        
+        var newPost = new Post(title, authorId, body);
+        await repository.AddAsync(newPost);
+        
+        Console.WriteLine($"Post med titlen '{newPost.Title}' er nu oprettet");
     }
 }
