4bd03aa37c77ded26c4d715d6cea95dda401a577
KatAamand <katrine.aamand@icloud.com>
2024-09-17 21:09:06 +0200

    Opdater med filtreringsmuligheder og show() metode

---
+++ Server/CLI/UI/ManageUsers/ListUserView.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace CLI.UI.ManageUsers;
+
+public class ListUserView
+{
+    private readonly IRepository<User> repository;
+
+    public ListUserView(IRepository<User> repository)
+    {
+        this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    public void Show()
+    {
+        throw new NotImplementedException();
+    }
+}
