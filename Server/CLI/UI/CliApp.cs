4470e62fa6e78b1c98753627deb2c343c94ba1ad
KatAamand <katrine.aamand@icloud.com>
2024-09-17 20:02:43 +0200

    adding constructor and startAsync method to CliApp

---
+++ Server/CLI/UI/CliApp.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace CLI.UI;
+
+public class CliApp
+{
+    IRepository<User> userRepository;
+    IRepository<Post> postRepository;
+    IRepository<Comment> commentRepository;
+
+    public CliApp(IRepository<User> userRepository, IRepository<Post> postRepository, IRepository<Comment> commentRepository)
+    {
+        this.userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        this.postRepository = postRepository ?? throw new ArgumentNullException(nameof(postRepository));
+        this.commentRepository = commentRepository ?? throw new ArgumentNullException(nameof(commentRepository));
+    }
+
+    public async Task StartAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
