7dba892127f9d475b85cccbda83b12814bd314b5
KatAamand <katrine.aamand@icloud.com>
2024-09-17 21:09:32 +0200

    Tilføjer CLI menu

--- Server/CLI/UI/CliApp.cs
+++ Server/CLI/UI/CliApp.cs
@@ -1,4 +1,6 @@
-﻿using Entities;
+﻿using CLI.UI.ManagePosts;
+using CLI.UI.ManageUsers;
+using Entities;
 using RepositoryContracts;
 
 namespace CLI.UI;
@@ -18,6 +20,48 @@ public class CliApp
 
     public async Task StartAsync()
     {
-        throw new NotImplementedException();
+        bool running = true;
+
+        while (running)
+        {
+            Console.Clear();
+            Console.WriteLine("Velkommen til det skæve forum!");
+            Console.WriteLine("Hvad skal der ske?");
+            Console.WriteLine("1. Se posts");
+            Console.WriteLine("2. Opret ny post");
+            Console.WriteLine("3. Opret ny bruger");
+            Console.WriteLine("4. Se alle brugere");
+            Console.WriteLine("5. Afslut");
+            
+            Console.WriteLine("Indtast valg fra 1 - 5");
+            string? input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    var listPostsView = new ListPostsView(postRepository); // Viser dagens posts
+                    listPostsView.ShowFilterMenu();
+                    break;
+                case "2":
+                    var createPostView = new CreatePostView(postRepository); // Opret ny post
+                    createPostView.Show();
+                    break;
+                case "3":
+                    var createUserView = new CreateUserView(userRepository); // Opret ny bruger
+                    createUserView.Show();
+                    break;
+                case "4":
+                    var listUserView = new ListUserView(userRepository); // Se alle brugere
+                    listUserView.Show();
+                    break;
+                case "5":
+                    running = false; // Afslut programmet
+                    break;
+                default:
+                    Console.WriteLine("Ugyldigt valg, prøv igen.");
+                    break;
+            }
+            
+        }
     }
 }
