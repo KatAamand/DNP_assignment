b01dcf4b6bdc2003a959f90e615715a851a4924b
KatAamand <katrine.aamand@icloud.com>
2024-10-08 20:23:12 +0200

    adding filepaths and filerepo

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -2,15 +2,20 @@
 
 using CLI.UI;
 using Entities;
-using InMemoryRepositories;
+using FileIORepositories;
 using RepositoryContracts;
 
 Console.WriteLine("Start CLI app");
 
+// json filestrings
+string userRepositoryFilepath = "UserRepo.json"; 
+string postRepositoryFilepath = "PostRepo.json";
+string commentRepositoryFilepath = "CommentRepo.json";
+
 // Opretter repositories med dummydata
-IRepository<User> userRepository = new InMemoryRepository<User>();
-IRepository<Post> postRepository = new InMemoryRepository<Post>();
-IRepository<Comment> commentRepository = new InMemoryRepository<Comment>();
+IRepository<User> userRepository = new FileRepository<User>(userRepositoryFilepath);
+IRepository<Post> postRepository = new FileRepository<Post>(postRepositoryFilepath);
+IRepository<Comment> commentRepository = new FileRepository<Comment>(commentRepositoryFilepath);
 
 CliApp cliApp = new CliApp(userRepository, postRepository, commentRepository); 
 await cliApp.StartAsync(); 
