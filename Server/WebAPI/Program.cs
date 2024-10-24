6e41e41efcbc1ad3ce8af9b3fdd5da1488de591c
KatAamand <katrine.aamand@icloud.com>
2024-10-24 17:19:33 +0200

    Updating program.cs and csproj file for WebAPI

--- Server/WebAPI/Program.cs
+++ Server/WebAPI/Program.cs
@@ -1,3 +1,7 @@
+using Entities;
+using FileIORepositories;
+using RepositoryContracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRepository<Post>, FileRepository<Post>>();
+builder.Services.AddScoped<IRepository<User>, FileRepository<User>>();
+builder.Services.AddScoped<IRepository<Comment>, FileRepository<Comment>>();
 
 var app = builder.Build();
 
