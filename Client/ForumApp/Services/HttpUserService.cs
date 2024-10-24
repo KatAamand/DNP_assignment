cb52bda54e172299cd550cd25e164e7a6f83a1fa
KatAamand <katrine.aamand@icloud.com>
2024-10-24 18:08:18 +0200

    Adding Forum Web app and adding CreateUser service

---
+++ Client/ForumApp/Services/HttpUserService.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using DTO;
+
+namespace ForumApp.Services;
+
+public class HttpUserService : IUserService
+{
+    private readonly HttpClient _httpClient;
+
+    public HttpUserService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<UserDTO> AddUserAsync(CreateUserDTO user)
+    {
+        HttpResponseMessage httpResponse = await _httpClient.PostAsJsonAsync("users", user);
+        string response = await httpResponse.Content.ReadAsStringAsync();
+        if (!httpResponse.IsSuccessStatusCode)
+        {
+            throw new Exception(response);
+        }
+        return JsonSerializer.Deserialize<UserDTO>(response, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+    }
+
+    public async Task UpdateUserAsync(int id, UserDTO user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteUserAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<UserDTO> GetUserAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<UserDTO>> GetUsersAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Client/ForumApp/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using DTO;
+
+namespace ForumApp.Services;
+
+public interface IUserService
+{
+    public Task<UserDTO> AddUserAsync(CreateUserDTO user);
+    public Task UpdateUserAsync(int id, UserDTO user);
+    public Task<bool> DeleteUserAsync(int id);
+    public Task<UserDTO> GetUserAsync(int id);
+    public Task<List<UserDTO>> GetUsersAsync();
+}
