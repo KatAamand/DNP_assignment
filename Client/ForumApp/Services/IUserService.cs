f9b58ef579a92fe1fc9ac2dda32b77d7e3a9ed0c
KatAamand <katrine.aamand@icloud.com>
2024-10-28 22:45:57 +0100

    Update AddUserAsync method and adding Create User possibility in navigation bar

--- Client/ForumApp/Services/IUserService.cs
+++ Client/ForumApp/Services/IUserService.cs
@@ -4,7 +4,7 @@ namespace ForumApp.Services;
 
 public interface IUserService
 {
-    public Task<UserDTO> AddUserAsync(CreateUserDTO user);
+    public Task<bool> AddUserAsync(string username, string password);
     public Task UpdateUserAsync(int id, UserDTO user);
     public Task<bool> DeleteUserAsync(int id);
     public Task<UserDTO> GetUserAsync(int id);
